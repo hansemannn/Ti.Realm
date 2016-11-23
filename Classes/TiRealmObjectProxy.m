eda5f634cfd8f9145ee0ac1aa291e7e202990973
Hans Knöchel <hans.knoechel@hs-osnabrueck.de>
2016-11-23 00:04:25 -0600

    Support for dynamic property swizzling

--- Classes/TiRealmObjectProxy.m
+++ Classes/TiRealmObjectProxy.m
@@ -8,6 +8,7 @@
 #import "TiRealmObjectProxy.h"
 #import "TiRealmObject.h"
 #import "TiUtils.h"
+#import <objc/runtime.h>
 
 @implementation TiRealmObjectProxy
 
@@ -15,15 +16,38 @@
 
 - (id)_initWithPageContext:(id<TiEvaluator>)context andName:(NSString*)name attributes:(NSDictionary*)attributes
 {
+    // TODO: Map types to Realm constants
+    
     if (self = [super _initWithPageContext:context]) {
-        
-        // TODO: Set name of object to the name provided
-        // TODO: Hack properties from proxy into object (Reflection?)
-        // TODO: Cast attribute values to native ones and assign using `initWithValue:` afterwards
+        [TiRealmObjectProxy manipulateClassWithAttributes:attributes];
         [self setObject:[[TiRealmObject alloc] init]];
     }
     
     return self;
+}
+    
++ (void)manipulateClassWithAttributes:(NSDictionary*)attributes
+{
+        for (NSString* key in attributes) {
+            objc_property_attribute_t type = { "T", [[NSString stringWithFormat:@"@\"%@\"",[attributes valueForKey:key]] UTF8String] };
+            objc_property_attribute_t backingivar  = { "V", "_privateName" };
+            objc_property_attribute_t attrs[] = { type, backingivar };
+            
+            class_addProperty([TiRealmObject class], [key UTF8String], attrs, 2);
+            class_addMethod([TiRealmObject class], NSSelectorFromString(key), (IMP)nameGetter, "@@:");
+            class_addMethod([TiRealmObject class], NSSelectorFromString([NSString stringWithFormat:@"%@:", key]), (IMP)nameSetter, "v@:@");
+        }
+}
+
+NSString *nameGetter(id self, SEL _cmd) {
+    Ivar ivar = class_getInstanceVariable([TiRealmObject class], "_privateName");
+    return object_getIvar(self, ivar);
+}
+
+void nameSetter(id self, SEL _cmd, NSString *newName) {
+    Ivar ivar = class_getInstanceVariable([TiRealmObject class], "_privateName");
+    id oldName = object_getIvar(self, ivar);
+    if (oldName != newName) object_setIvar(self, ivar, [newName copy]);
 }
 
 #pragma mark Public APIs
